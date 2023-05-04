d7d86239ba03f72e1f18315d52872343ef4567b0
MariaSora <mariaperarnausanguesa@gmail.com>
2023-05-04 18:24:00 +0200

    no va

--- Main/Sorcer Striker/Source/ScenePlayerSelect.cpp
+++ Main/Sorcer Striker/Source/ScenePlayerSelect.cpp
@@ -20,71 +20,68 @@ ScenePlayerSelect::~ScenePlayerSelect()
 // Load assets
 bool ScenePlayerSelect::Start()
 {
-	LOG("Loading background assets");
+    LOG("Loading background assets");
 
-	bool ret = true;
+    bool ret = true;
 
-	Texture1 = App->textures->Load("Assets/Sprites/PlayerSelectt.png");
-	
-	//App->render->Blit(Texture1, -246, 2, NULL); Para saber las coordenadas de los sprites
-	//App->render->Blit(Texture1, -492, 0, NULL);
-	//App->render->Blit(Texture1, -738, 0, NULL);
+    Texture1 = App->textures->Load("Assets/Sprites/PlayerSelect.png");
 
-	App->audio->PlayMusic("Assets/Music/characterSelect.ogg", 1.0f);
-	PowerUpCollectFx = App->audio->LoadFx("Assets/Fx/powerUp.wav");
 
-	App->render->camera.x = 0;
-	App->render->camera.y = 0;
 
-	return ret;
+    App->audio->PlayMusic("Assets/Music/characterSelect.ogg", 1.0f);
+    PowerUpCollectFx = App->audio->LoadFx("Assets/Fx/powerUp.wav");
+
+    App->render->camera.x = 0;
+    App->render->camera.y = 0;
+
+
+    return ret;
 }
 
 update_status ScenePlayerSelect::Update()
 {
+    if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
+    {
+        App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
+        App->audio->PlayFx(PowerUpCollectFx);
+    }
 
-	if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
-	{
-		App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
-		App->audio->PlayFx(PowerUpCollectFx);
-	}
-	
 
-	return update_status::UPDATE_CONTINUE;
+    return update_status::UPDATE_CONTINUE;
 }
 
 // Update: draw background
 
 update_status ScenePlayerSelect::PostUpdate()
 {
-	// Draw everything --------------------------------------
-	int x = 0;
-	int y = 0;
-	//App->render->Blit(Texture1, x, y, NULL);
-	App->render->Blit(Texture1, x, y, NULL);
-	
-	if (App->input->keys[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT)
-	{
-		//if (x >= -738 && x <= 0) {
-		//	x == -738;
-		//}
-		//else{ x += 246; }
-		//
-		////y -= 2;
-		App->render->Blit(Texture1, -246, 2, NULL);
-	}
+    // Draw everything --------------------------------------
+    int x = 0;
 
-	if (App->input->keys[SDL_SCANCODE_D] == KEY_STATE::KEY_REPEAT)
-	{
-		//if (x >= -738 && x <= 0) {
-		//	x == 0;
-		//}
-		//else{ x -= 246; }
-		//
-		////y += 2;
-		App->render->Blit(Texture1, -492, 0, NULL);
-	}
+    //App->render->Blit(Texture1, -246, 2, NULL); Para saber las coordenadas de los sprites
+    //App->render->Blit(Texture1, -492, 0, NULL);
+    //App->render->Blit(Texture1, -738, 0, NULL);
 
-	
+    App->render->Blit(Texture1, x, 0, NULL);
 
-	return update_status::UPDATE_CONTINUE;
+    if (App->input->keys[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT)
+    {
+        //App->textures->Unload(Texture1);
+        if (x < 0) {
+            x += 246;
+        }
+        App->render->Blit(Texture1, x, 0, NULL);
+    }
+
+    if (App->input->keys[SDL_SCANCODE_D] == KEY_STATE::KEY_REPEAT)
+    {
+        //App->textures->Unload(Texture1);
+        if (x > -978) {
+            x -= 246;
+        }
+        App->render->Blit(Texture1, x, 0, NULL);
+    }
+
+
+
+    return update_status::UPDATE_CONTINUE;
 }
